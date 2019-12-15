1fccba2549f65ad51a1c26e5ad301d4ba31b1e99
Glitch (rare-fuel) <none>
2019-12-15 07:57:54 +0000

    🌚💖 Updated with Glitch

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-// Requires
 const express = require('express');
 const bodyParser = require('body-parser');
 const expect = require('chai').expect;
@@ -9,13 +7,11 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 
-// Testing Requires
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
 
-// Express
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -25,38 +21,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-// Helmet
 app.use(helmet());
 app.use(helmet.noSniff());
 app.use(helmet.xssFilter());
 
 
-
-// Express Routes
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-// // Convert
-// app.route('/api/convert/')
-//   .get((req, res) => {
-  
-//     let length = req.url.length;
-//     let input = req.url.slice(19, length);
-  
-//     console.log(input)
-//     let obj = {TEST: input ,initNum: 3.1, initUnit: 'mi', returnNum: 5.0000008, returnUnit: 'km', string: '3.1 miles converts to 5.00002 kilometers'}
-//     res.send(obj);
-// });
-
-
-
-//For FCC testing purposes
 fccTestingRoutes(app);
-//Routing for API 
 apiRoutes(app);  
-//404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
     .type('text')
@@ -64,9 +40,6 @@ app.use(function(req, res, next) {
 });
 
 
-
-
-// Server Listener
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
   if(process.env.NODE_ENV==='test') {
@@ -83,4 +56,4 @@ app.listen(process.env.PORT || 3000, function () {
   }
 });
 
-module.exports = app; //for testing
+module.exports = app; 
